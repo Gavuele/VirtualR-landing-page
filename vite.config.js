152a151b0964a9e07e6dd2b7280366f1e1caac5d
Your Name <you@example.com>
2024-06-12 15:47:07 +0300

    deploy commit

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['lucide-react'],
-    },
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
-    },
-  }
 });
